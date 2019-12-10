038861ba3ba3a367392fd1adac07162900ad31cd
MorganNkosi <54417130+MorganNkosi@users.noreply.github.com>
2019-12-10 11:31:55 +0200

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -3,36 +3,38 @@ import java.util.Scanner;
 public class arrays {
     public static void main(String[] args) {
 
-        System.out.println("Please enter the size of your Arrays: ");
+        System.out.println("Enter Array size : ");
 
         Scanner input = new Scanner(System.in);
-        int amount = input.nextInt();
+        int unitSize = input.nextInt();
 
-        System.out.println("Please Enter "+amount+ " Strings for your 1st Array");
+        System.out.println("Enter "+unitSize+ " Strings for your 1st Array");
 
         Scanner scanner1 = new Scanner(System.in);
-        String[] firstArray = new String[amount];
+        String[] firstArray = new String[unitSize];
 
         for (int i = 0; i < firstArray.length; i++) {
             firstArray[i] = scanner1.nextLine();
         }
 
-        System.out.println("Please Enter "+amount+ " Strings for your 2nd Array");
+        System.out.println("Enter " + unitSize + " Strings for your 2nd Array");
 
-        Scanner scanner2 = new Scanner(System.in);
-        String[] secondArray = new String[amount];
+        
+        String[] secondArray = new String[unitSize];
 
-        for (int i = 0; i < secondArray.length; i++) {
-            secondArray[i] = scanner2.nextLine();
+        for (int i = 0; i < secondArray.length; i++)
+        {
+            secondArray[i] = scanner1.nextLine();
         }
-
-        //  Output
-        for (int i = 0; i < firstArray.length; i++) {
-            System.out.print(firstArray[i] + " "+ secondArray[i] + " ");
-        }
-
-
+        combine(firstArray,secondArray);
     }
 
-
+    public static void combine(String firstArray[], String secondArray[])
+    {
+        for (int i = 0; i < firstArray.length; i++)
+        {
+            System.out.print(firstArray[i] + " " + secondArray[i] + " ");
+        }
+    }
 }
+
